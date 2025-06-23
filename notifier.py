be65e34068b1dc3d54f5324495fbc0bab2482f31
Shohjahon <sh.murodovv@gmail.com>
2025-06-23 14:57:46 +0500

    Created function for sending msg

---
+++ notifier.py
@@ -0,0 +1,3 @@
+def send_notification(email, phone, room, start, end):
+    print(f"EMAIL to {email}: Вы забронировали кабинет {room} с {start} до {end}")
+    print(f"SMS to {phone}: Бронирование кабинета {room} с {start} до {end}")
